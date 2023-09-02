9d2ff8a97089e84a2dff4686d799f3858a9d846f
Kenyg369 <73320411+Kenyg369@users.noreply.github.com>
2023-09-02 15:07:00 +0800

    merge test: master 1

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
-# branch 
-this is branch creation 
+# branch,this is branch creation 
 
-# branch 1 
-this is branch one 1
+class BinaryPerceptron(object):
+
+    def __init__(self, examples, iterations):
+        self.examples = examples
+        self.iterations = iterations
+        # See dimension (length of longest x_vector)
+        d = 0
+        for example in examples:
+            x = example[0]
+            if len(x) > d:
+                d = len(example)
+        # Build all zero initial omega
+        self.omega = [0 for _ in range(d)]
+        # self.omegas = dict()
+        # self.omegas[True] = [0 for _ in range(d)]
+        # self.omegas[False] = [0 for _ in range(d)]
+        # Train the classifier for iterations times
+        for _ in range(self.iterations):
+            # print("Round ", _, " iteration")
+            for example in self.examples:
+                # print(example)
+                x = example[0]
+                y = example[1]
+                y_hat = self.predict(x)
+                if y_hat != y:
+                    # print("y_hat: ", y_hat, " != ", "y: ", y)
+                    self.omega_plus_x(example)
+                    
+
+    def predict(self, x):
+        # score_true = 0
+        # score_false = 0
+        product = 0
+        for key, value in x.items():
+            d = int(key[1:])
+            product = product + self.omega[d-1] * value
+            # score_true = score_true + self.omegas[True][d-1] * value
+            # score_false = score_false + self.omegas[False][d-1] * value
+        if product > 0:
+            return True
+        else:
+            return False
+
+
+    def omega_plus_x(self, example):
+        x = example[0]
+        y = example[1]
+        for key, value in x.items():
+            d = int(key[1:])
+            if y is True:
+                self.omega[d-1] = self.omega[d-1] + value
+            else:
+                self.omega[d-1] = self.omega[d-1] - value
+        # print("new omega: ", self.omega)
